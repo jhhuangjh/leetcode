09e722574cbe8cfd2f4edfc7ae3f71a5f26b2233
jhhuangjh <1553851409@qq.com>
2020-08-06 21:43:50 +0800

    添加了一个hello类

---
+++ src/org/jh/queue/Hello.java
@@ -0,0 +1,12 @@
+package org.jh.queue;
+
+/**
+ * @author hjh
+ * @version 1.0
+ * @date 2020/8/6 21:42
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
