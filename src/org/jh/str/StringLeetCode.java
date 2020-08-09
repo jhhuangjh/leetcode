a2a890610702150903b368bbd53402fe05abbf88
jhhuangjh <1553851409@qq.com>
2020-08-09 14:43:00 +0800

    2020.08.09字符串新增路径旅行终点站

--- src/org/jh/str/StringLeetCode.java
+++ src/org/jh/str/StringLeetCode.java
@@ -1,5 +1,9 @@
 package org.jh.str;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author hjh
  * @version 1.0
@@ -45,6 +49,31 @@ public class StringLeetCode {
             }
         }
         return sb.toString();
+    }
+
+
+    /**
+     * 给你一份旅游线路图，该线路图中的旅行线路用数组 paths 表示，其中 paths[i] = [cityAi, cityBi]
+     * 表示该线路将会从 cityAi 直接前往 cityBi 。
+     * 请你找出这次旅行的终点站，即没有任何可以通往其他城市的线路的城市。
+     *
+     * @param paths paths = [["B","C"],["D","B"],["C","A"]]
+     * @return "A"
+     * @date 2020/08/09
+     */
+    public String destCity(List<List<String>> paths) {
+        Map<String, String> map = new HashMap<>();
+        // 将所有路径的起点与终点存入map
+        for (List<String> path : paths) {
+            map.put(path.get(0), path.get(1));
+        }
+        for (String key : map.keySet()) {
+            // 如果某个路径的终点不是另一个路径的起点，则该点就是旅行终点
+            if (map.get(map.get(key)) == null) {
+                return map.get(key);
+            }
+        }
+        return "";
     }
 
 
