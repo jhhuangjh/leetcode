0580bd7e977e90749dfc898aa40adf90ebc2b3cf
jhhuangjh <1553851409@qq.com>
2020-08-15 21:36:28 +0800

    2020.08.15字符串新增字符串相乘

--- src/org/jh/str/StringLeetCode.java
+++ src/org/jh/str/StringLeetCode.java
@@ -184,5 +184,40 @@ public class StringLeetCode {
         return a == 0 && b == 0;
     }
 
+    /**
+     * 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+     * num1 和 num2 的长度小于110。
+     * num1 和 num2 只包含数字 0-9。
+     * num1 和 num2 均不以零开头，除非是数字 0 本身。
+     * 不能使用任何标准库的大数类型（比如 BigInteger）或直接将输入转换为整数来处理。
+     *
+     * @param num1 num1 = "2"
+     * @param num2 num2 = "3"
+     * @return "6"
+     */
+    public String multiply(String num1, String num2) {
+        if ("0".equals(num1) || "0".equals(num2)) return "0";
+        int len1 = num1.length();
+        int len2 = num2.length();
+        int[] ansArr = new int[len1 + len2];
+        for (int i = len1 - 1; i >= 0; i--) {
+            int x = num1.charAt(i) - '0';
+            for (int j = len2 - 1; j >= 0; j--) {
+                int y = num2.charAt(j) - '0';
+                ansArr[i + j + 1] += x * y;
+            }
+        }
+        for (int i = len1 + len2 - 1; i > 0; i--) {
+            ansArr[i - 1] += ansArr[i] / 10;
+            ansArr[i] %= 10;
+        }
+        StringBuilder sb = new StringBuilder();
+        int index = ansArr[0] == 0 ? 1 : 0;
+        while (index < len1 + len2) {
+            sb.append(ansArr[index++]);
+        }
+        return sb.toString();
+    }
+
 
 }
