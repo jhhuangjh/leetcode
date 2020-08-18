89cbf1adde0e43c7da895971e5699ecc811cf9a1
jhhuangjh <1553851409@qq.com>
2020-08-18 21:19:44 +0800

    2020.08.19字符串新增 1309.解码字母到整数映射

--- src/org/jh/str/StringLeetCode.java
+++ src/org/jh/str/StringLeetCode.java
@@ -219,5 +219,30 @@ public class StringLeetCode {
         return sb.toString();
     }
 
+    /**
+     * 1309. 解码字母到整数映射
+     * 给你一个字符串 s，它由数字（'0' - '9'）和 '#' 组成。
+     * 我们希望按下述规则将 s 映射为一些小写英文字符：
+     * 字符（'a' - 'i'）分别用（'1' - '9'）表示。
+     * 字符（'j' - 'z'）分别用（'10#' - '26#'）表示。
+     * 返回映射之后形成的新字符串。
+     *
+     * @param s s = "10#11#12"
+     * @return "jkab"
+     * @date 2020.08.18
+     */
+    public String freqAlphabets(String s) {
+        int len = s.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            if (i + 2 < len && s.charAt(i + 2) == '#') {
+                sb.append((char) (Integer.parseInt(s.substring(i, i + 2)) - 1 + 'a'));
+                i += 2;
+            } else {
+                sb.append((char) (s.charAt(i) - '1' + 'a'));
+            }
+        }
+        return sb.toString();
+    }
 
 }
