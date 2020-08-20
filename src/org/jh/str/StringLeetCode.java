b171f7041b4bde887bf7e89897e925d21ec38d2e
jhhuangjh <1553851409@qq.com>
2020-08-20 23:16:49 +0800

    2020.08.20字符串新增 1370.上升下降字符串

--- src/org/jh/str/StringLeetCode.java
+++ src/org/jh/str/StringLeetCode.java
@@ -245,4 +245,79 @@ public class StringLeetCode {
         return sb.toString();
     }
 
+    /**
+     * 1370.上升下降字符串
+     * 给你一个字符串 s ，请你根据下面的算法重新构造字符串：
+     * 从 s 中选出 最小 的字符，将它 接在 结果字符串的后面。
+     * 从 s 剩余字符中选出 最小 的字符，且该字符比上一个添加的字符大，将它 接在 结果字符串后面。
+     * 重复步骤 2 ，直到你没法从 s 中选择字符。
+     * 从 s 中选出 最大 的字符，将它 接在 结果字符串的后面。
+     * 从 s 剩余字符中选出 最大 的字符，且该字符比上一个添加的字符小，将它 接在 结果字符串后面。
+     * 重复步骤 5 ，直到你没法从 s 中选择字符。
+     * 重复步骤 1 到 6 ，直到 s 中所有字符都已经被选过。
+     * 在任何一步中，如果最小或者最大字符不止一个 ，你可以选择其中任意一个，并将其添加到结果字符串。
+     * <p>
+     * 请你返回将 s 中字符重新排序后的 结果字符串 。
+     *
+     * @param s s = "aaaabbbbcccc"
+     * @return "abccbaabccba"
+     * @date 2020.08.20
+     */
+    /*private class SortString {
+        int[] arr = new int[26];
+        int len = arr.length;
+        StringBuilder sb = new StringBuilder();
+
+        public String sortString(String s) {
+            for (char c : s.toCharArray()) {
+                arr[c - 'a']++;
+            }
+            while (haveChar()) {
+                for (int i = 0; i < len; i++) {
+                    appendChar(i);
+                }
+                for (int i = 25; i >= 0; i--) {
+                    appendChar(i);
+                }
+            }
+            return sb.toString();
+        }
+
+        private boolean haveChar() {
+            for (int value : arr) {
+                if (value > 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void appendChar(int index) {
+            if (arr[index] > 0) {
+                arr[index]--;
+                sb.append((char) (index + 'a'));
+            }
+        }
+    }*/
+    public String sortString(String s) {
+        int[] arr = new int[26];
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            arr[c - 'a']++;
+        }
+        while (sb.length() != s.length()) {
+            for (int i = 0; i < 26; i++) {
+                if (arr[i] == 0) continue;
+                sb.append((char) (i + 'a'));
+                arr[i]--;
+            }
+            for (int i = 25; i >= 0; i--) {
+                if (arr[i] == 0) continue;
+                sb.append((char) (i + 'a'));
+                arr[i]--;
+            }
+        }
+        return sb.toString();
+    }
+
 }
