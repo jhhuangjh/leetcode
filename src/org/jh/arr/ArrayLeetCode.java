7ad71a5d7b7f73be9fb0cd5e390d37f4e7ea2c28
jhhuangjh <1553851409@qq.com>
2020-08-20 21:49:21 +0800

    2020.08.20数组新增 1266.访问所有点的最小时间 切比雪夫距离

--- src/org/jh/arr/ArrayLeetCode.java
+++ src/org/jh/arr/ArrayLeetCode.java
@@ -2,8 +2,7 @@ package org.jh.arr;
 
 import java.util.*;
 
-import static java.lang.Math.log;
-import static java.lang.Math.log10;
+import static java.lang.Math.*;
 
 /**
  * @author hjh
@@ -236,6 +235,26 @@ public class ArrayLeetCode {
             }
         }
         return count;
+    }
+
+    /**
+     * 1266. 访问所有点的最小时间
+     * 平面上有 n 个点，点的位置用整数坐标表示 points[i] = [xi, yi]。请你计算访问所有这些点需要的最小时间（以秒为单位）。
+     * 你可以按照下面的规则在平面上移动：
+     * 每一秒沿水平或者竖直方向移动一个单位长度，或者跨过对角线（可以看作在一秒内向水平和竖直方向各移动一个单位长度）。
+     * 必须按照数组中出现的顺序来访问这些点。
+     *
+     * @param points points = [[1,1],[3,4],[-1,0]]
+     * @return 7
+     * @date 2020.08.20
+     */
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int result = 0;
+        int len = points.length;
+        for (int i = 0; i < len - 1; ++i) {
+            result += Math.max(Math.abs(points[i][0] - points[i + 1][0]), Math.abs(points[i][1] - points[i + 1][1]));
+        }
+        return result;
     }
 
 
