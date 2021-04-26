debee54e4414e27a0fde729ebe2b46ea1ec09ac8
jhhuangjh <1553851409@qq.com>
2021-04-27 00:29:21 +0800

    2021.04.27自己实现ArrayList的add方法

---
+++ src/org/jh/learn/Main.java
@@ -0,0 +1,19 @@
+package org.jh.learn;
+
+import java.util.ArrayList;
+
+/**
+ * @author hjh
+ * @version 1.0
+ * @date 2021/4/27 0:04
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyArrayList<String> list = new MyArrayList<>(0);
+        for (int i = 0; i < 1000; i++) {
+            list.add(""+ i);
+        }
+        System.out.println(list.size());
+        System.out.println(2 + (2>>1));
+    }
+}
---
+++ src/org/jh/learn/MyArrayList.java
@@ -0,0 +1,9 @@
+package org.jh.learn;
+
+/**
+ * @author hjh
+ * @version 1.0
+ * @date 2021/4/26 23:27
+ */
+public class MyArrayList {
+}
