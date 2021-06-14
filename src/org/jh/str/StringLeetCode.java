af09cd11f78bdcb06461d509bb42045a2478d4a3
jhhuangjh <1553851409@qq.com>
2021-06-14 14:35:06 +0800

    2021.06.14字符串新增翻转单词顺序

--- src/org/jh/str/StringLeetCode.java
+++ src/org/jh/str/StringLeetCode.java
@@ -454,4 +454,28 @@ public class StringLeetCode {
         return sb.toString().length() < S.length() ? sb.toString() : S;
     }
 
+    /**
+     * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。
+     * 为简单起见，标点符号和普通字母一样处理。
+     * 例如输入字符串"I am a student. "，则输出"student. a am I"。
+     *
+     * @param s "I am a student. "
+     * @return "student. a am I"
+     */
+    public static String reverseWords1(String s) {
+        s = s.trim(); // 删除首尾空格
+        int j = s.length() - 1, i = j;
+        StringBuilder res = new StringBuilder();
+        while(i >= 0) {
+            while(i >= 0 && s.charAt(i) != ' ') {
+                i--; // 搜索首个空格
+            }
+            res.append(s.substring(i + 1, j + 1)).append(" "); // 添加单词
+            while(i >= 0 && s.charAt(i) == ' ') {
+                i--; // 跳过单词间空格
+            }
+            j = i; // j 指向下个单词的尾字符
+        }
+        return res.toString().trim(); // 转化为字符串并返回
+    }
 }
