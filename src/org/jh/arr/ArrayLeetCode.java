531d8249b365459ff31f2ed68add2f1632ba9704
jhhuangjh <1553851409@qq.com>
2021-07-04 18:32:46 +0800

    2021.07.04数组新增错误的集合

--- src/org/jh/arr/ArrayLeetCode.java
+++ src/org/jh/arr/ArrayLeetCode.java
@@ -345,4 +345,32 @@ public class ArrayLeetCode {
         }
         return -1;
     }
+
+
+    /**
+     * 集合 s 包含从 1 到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个数字复制了成了集合里面的另外一个数字的值，
+     * 导致集合 丢失了一个数字 并且 有一个数字重复 。
+     * 给定一个数组 nums 代表了集合 S 发生错误后的结果。
+     * 请你找出重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+     *
+     * @param nums [1,2,2,4]
+     * @return [2,3]
+     */
+    public int[] findErrorNums(int[] nums) {
+        int[] errorNums = new int[2];
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int i = 1; i <= n; i++) {
+            int count = map.getOrDefault(i, 0);
+            if (count == 2) {
+                errorNums[0] = i;
+            } else if (count == 0) {
+                errorNums[1] = i;
+            }
+        }
+        return errorNums;
+    }
 }
